c47727e0fb3be5d6d09ca2d4ebe05575543c53a8
mespn <manuelen2002@gmail.com>
2025-01-16 11:49:44 -0600

    Initial commit backend

---
+++ backend/src/test/java/me/magicbudget/magicbudget/MagicbudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package me.magicbudget.magicbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicbudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
