bbb65a138d82e005bbf3287fe85c9d667685a1a8
mespn <manuelen2002@gmail.com>
2025-02-06 13:05:37 -0600

    add category property to Transaction class

--- frontend/src/app/transactions/(objects)/transaction.ts
+++ frontend/src/app/transactions/(objects)/transaction.ts
@@ -4,18 +4,21 @@ export default class Transaction{
     name: string;
     amount: number;
     description:string;
+    category:string;
 
     constructor(
         id:string,
         date:string,
         name:string,
         amount:number,
+        category:string,
         description:string
     ){
         this.id = id
         this.date= new Date(date);
         this.name= name;
         this.amount= amount;
+        this.category= category;
         this.description= description;
     }
 
