b64e663ebb0474f986ab1b7ecaefc1be5577c59d
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2025-02-25 10:30:23 -0600

    chore: refactor to using props

--- frontend/src/app/goals/goalsList.tsx
+++ frontend/src/app/goals/goalsList.tsx
@@ -1,50 +1,46 @@
 import { Progress } from "@/components/ui/progress";
 import Goal from "./objects/goal";
 
+interface GoalsListProps {
+  goals: Goal[];
+}
 
-const today = new Date().toISOString().split('T')[0]
-const mockGoals = [
-    new Goal("1", today, "House",140000, 300000),
-    new Goal("2", today, "PC upgrade", 100, 2000),
-    new Goal("3", today, "Vacation", 4300, 5000),
-];
-
-export default async function GoalsList() {
-    return (
+export default function GoalsList({ goals }: GoalsListProps) {
+  return (
     <div>
-        {
-            mockGoals.map((goal: Goal) =>(
-            <div className="pt-6" key = {goal.id}>
-
-                <div className="bg-gray-100 rounded-xl p-6 shadow-md w-full h-48">
-                    <div >
-                        <div className="" >
-                            <h1 className="text-3xl"> Goal: {goal.name}</h1>
-                        </div>
-                        <h1 className="text-xl pl-4 pt-2"> Save By: {goal.getDateString()}</h1>
-                    
-                    </div>
-
-                    <div className="pt-3">
-                        <Progress value={mapToProgress(goal.currAmount, goal.targetPrice)} className="bg-gray-300" />
-                        <div className="flex justify-between pt-2">
-                            <h1 className="text-xl pl-2"> ${goal.currAmount}</h1>
-                            <h1 className="text-xl pr-2"> ${goal.targetPrice} </h1>
-                        </div>
-                    </div>
-                </div>
+      {goals.map((goal: Goal) => (
+        <div className="pt-6" key={goal.id}>
+          <div className="bg-gray-100 rounded-xl p-6 shadow-md w-full h-48">
+            <div>
+              <div className="">
+                <h1 className="text-3xl"> Goal: {goal.name}</h1>
+              </div>
+              <h1 className="text-xl pl-4 pt-2">
+                {" "}
+                Save By: {goal.getDateString()}
+              </h1>
             </div>
-            ))
-        }
-    </div>
-    
-);
-};
 
-function mapToProgress(amount :number , target: number) {
-    let newVal : number;
-    newVal = amount *( 100/target);
-    newVal = Math.min(100, newVal);
-    return newVal
-    
+            <div className="pt-3">
+              <Progress
+                value={mapToProgress(goal.currAmount, goal.targetPrice)}
+                className="bg-gray-300"
+              />
+              <div className="flex justify-between pt-2">
+                <h1 className="text-xl pl-2"> ${goal.currAmount}</h1>
+                <h1 className="text-xl pr-2"> ${goal.targetPrice} </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function mapToProgress(amount: number, target: number) {
+  let newVal: number;
+  newVal = amount * (100 / target);
+  newVal = Math.min(100, newVal);
+  return newVal;
 }
--- frontend/src/app/goals/page.tsx
+++ frontend/src/app/goals/page.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import GoalsList from "./goalsList";
+import Goal from "./objects/goal";
+
+const today = new Date().toISOString().split("T")[0];
+const mockGoals = [
+  new Goal("1", today, "House", 140000, 300000),
+  new Goal("2", today, "PC upgrade", 100, 2000),
+  new Goal("3", today, "Vacation", 4300, 5000),
+];
+
 export default function Goals() {
-	return (
-		<div className="m-3">
-            <h1 className="text-4xl p-4"> Your goals</h1>
-			<div id="goals-container" className="flex">
-				<div className="w-full pl-8 pr-8">
-					<GoalsList/>
-				</div>
-
-			</div>
-
-		</div>
-	);
+  return (
+    <div className="m-3">
+      <h1 className="text-4xl p-4"> Your goals</h1>
+      <div id="goals-container" className="flex">
+        <div className="w-full pl-8 pr-8">
+          <GoalsList goals={mockGoals} />
+        </div>
+      </div>
+    </div>
+  );
 }
