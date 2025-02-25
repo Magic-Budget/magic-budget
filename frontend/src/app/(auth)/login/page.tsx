77dd423681a5b81ac2e7a564fb0c0f84a1597a7b
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2025-02-24 19:32:43 -0600

    chore: update page metadata

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -2,6 +2,11 @@
 // to create this all credit goes to the shadcn project.
 import { LoginForm } from "@/app/components/login-form";
 
+export const metadata = {
+  title: "Login",
+  description: "Log into your account",
+};
+
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
