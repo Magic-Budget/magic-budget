a31b09c5ecbeebc7f75d7ce36cba5c5b6f727d2b
mespn <manuelen2002@gmail.com>
2025-02-27 21:35:06 -0600

    Fix Query to filter by userID

--- backend/src/main/java/me/magicbudget/repository/CategoryRepository.java
+++ backend/src/main/java/me/magicbudget/repository/CategoryRepository.java
@@ -4,14 +4,17 @@ import java.util.List;
 import java.util.UUID;
 import me.magicbudget.model.Category;
 import me.magicbudget.model.CategoryTotals;
+import org.hibernate.annotations.Parameter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
 
   @Query("SELECT new me.magicbudget.model.CategoryTotals(c.name, SUM(t.amount)) "
-      + "FROM Category c JOIN Transaction t JOIN User u GROUP BY c.name")
-  List<CategoryTotals> findTotalAmountPerCategory();
+      + "FROM Category c JOIN Transaction t JOIN User u WHERE u.id = :userID "
+      + "GROUP BY c.name")
+  List<CategoryTotals> findTotalAmountPerCategory(@Param("userID") UUID userID);
 }
