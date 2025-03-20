fd5f94aecac797d9f98043a7700301f0ddf28ea6
Manuel Espinoza <manuelen2002@gmail.com>
2025-03-20 11:26:01 -0500

    Feat/split expense frontend (#111)
    * Change dto package names to match imports
    
    * Add necessary components for split form and create Multiselect component
    
    * Create expense split form
    
    * Use Form to implement split expense
    
    * Reduce repeated calls to API
    
    * Finish split expense frontend. Change API URLS as needed
    
    * Update splitForm.tsx
    
    * Fix formatting issues + code consistency
    
    * Wired to backend
    
    * Deleted f*ing mock data
    
    * Lint multiselect.tsx

--- backend/src/main/java/me/magicbudget/dto/incoming_request/IncomeRequest.java
+++ backend/src/main/java/me/magicbudget/dto/incoming_request/IncomeRequest.java
@@ -1,4 +1,4 @@
-package me.magicbudget.dto.incomingrequest;
+package me.magicbudget.dto.incoming_request;
 
 import me.magicbudget.model.IncomeType;
 import java.math.BigDecimal;
--- backend/src/main/java/me/magicbudget/dto/incoming_request/SavingGoalCreateRequest.java
+++ backend/src/main/java/me/magicbudget/dto/incoming_request/SavingGoalCreateRequest.java
@@ -1,4 +1,4 @@
-package me.magicbudget.dto.incomingrequest;
+package me.magicbudget.dto.incoming_request;
 
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
