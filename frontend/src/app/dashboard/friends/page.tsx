9e34dcd492ceab8a46eaf0037353a91259fb53bb
mespn <manuelen2002@gmail.com>
2025-03-21 05:39:54 -0500

    Create page.tsx

---
+++ frontend/src/app/dashboard/friends/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FriendsPage = () => {
+	return (
+		<div>
+			<div>
+				<h1>Friends</h1>
+				<p>Welcome to the friends page!</p>
+			</div>
+		</div>
+	);
+};
+
+export default FriendsPage;
