46ebde712cdd3657807d65bcbccebf0a9ab8ec34
Aaryaman Ranjit Pol <96331741+Tom24090119@users.noreply.github.com>
2025-03-21 00:25:32 -0500

    Split tracking (#109)
    * Added some features
    
    * done
    
    * Made changes that were requested
    
    * update project
    
    * Changes
    
    * Fixed Income bug
    
    * Fixing conflicts
    
    * Fixing conflicts
    
    * Fixing conflicts
    
    ---------
    
    Co-authored-by: Bruce <97542494+BruceNiemi@users.noreply.github.com>

---
+++ backend/src/main/java/me/magicbudget/dto/outgoing_response/FriendResponse.java
@@ -0,0 +1,11 @@
+package me.magicbudget.dto.outgoingresponse;
+
+import java.math.BigDecimal;
+
+public record FriendResponse(String username,
+                             String email,
+                        String name ) //,
+//                             BigDecimal amountOwed)
+                          {
+
+}
